ac76066c0c8db5504bd7fc9646c1d6e4394a2885
Grigory Frantsuzov <grigoriy.frantsuzov@quantori.com>
2024-06-13 17:22:55 +0300

    updated scripts

--- pllavaApi/get-model.ps1
+++ pllavaApi/get-model.ps1
@@ -4,14 +4,11 @@ $GIT_REPO_URL = "https://huggingface.co/ermu2001/pllava-7b.git"
 # Create MODELS directory if it doesn't exist
 New-Item -ItemType Directory -Path MODELS -ErrorAction SilentlyContinue
 
-# Clone the Git repository using Git LFS
-git lfs clone $GIT_REPO_URL MODELS
+# Clone the Git repository using Git LFS to MODELS\pllava-7b
+git lfs clone $GIT_REPO_URL MODELS\pllava-7b
 
-# Change directory to MODELS
-Set-Location -Path MODELS
-
-# Checkout the main branch
-git checkout main
+# Remove the .git folder after cloning
+Remove-Item -Path MODELS\pllava-7b\.git -Force -Recurse
 
 # Print success message
-Write-Output "Git PLLaVA-7B downloaded successfully to MODELS directory."
+Write-Output "Git PLLaVA-7B downloaded successfully to MODELS\pllava-7b directory."
