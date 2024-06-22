4c79065f2d0b4f8f72e9f50b5d86f5ad1d626bcf
Grigory Frantsuzov <grigoriy.frantsuzov@quantori.com>
2024-06-22 23:32:26 +0300

    similarity calculations

--- navecApi/main.py
+++ navecApi/main.py
@@ -1,6 +1,10 @@
+import numpy as np
 from fastapi import FastAPI, HTTPException
 from navec import Navec
-from typing import List, Dict
+from typing import List, Dict, Union
+from sklearn.metrics.pairwise import cosine_similarity
+from pydantic import BaseModel
+
 
 app = FastAPI()
 navec = Navec.load('navec_hudlit_v1_12B_500K_300d_100q.tar')
@@ -21,3 +25,27 @@ async def vectors(request: Dict[str, List[str]]):
             vector = []
         response.append({'word': word, 'vector': vector})
     return response
+
+
+
+
+class ClosestWordsRequest(BaseModel):
+    word: str
+    similarity_threshold: float = 0.0
+
+@app.post("/find_closest_words")
+async def find_closest_words(request: ClosestWordsRequest):
+    word = request.word.lower()  # Convert input word to lowercase
+    threshold = request.similarity_threshold
+    if word not in navec:
+        return {"error": f'Word "{word}" not found in dictionary.'}
+    
+    word_vector = navec[word].reshape(1, -1)  # Reshape for cosine_similarity
+    words = [w for w in navec.vocab.words if w != word]
+    vectors = np.array([navec[w] for w in words])
+    distances = cosine_similarity(word_vector, vectors).flatten()
+
+    closest_words_and_distances = [{"word": words[i], "sim": float(distances[i])} for i in range(len(distances)) if distances[i] >= threshold]
+    closest_words_and_distances.sort(key=lambda x: x["sim"], reverse=True)
+    
+    return closest_words_and_distances[:10]
